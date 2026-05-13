7efda9a5ca1ebb0f372dec1372f6b6d4bb7ea991
Goats Temple • <barisbilgili00@gmail.com>
2026-05-13 06:48:46 +0300

    Update LicenseUtils.java

--- app/src/main/java/com/bycomsolutions/bycomvpn/utils/LicenseUtils.java
+++ app/src/main/java/com/bycomsolutions/bycomvpn/utils/LicenseUtils.java
@@ -5,7 +5,7 @@ import android.app.Activity;
 public class LicenseUtils {
 
     public static void verifyLicense(Activity activity) {
-        // GoatBorg v2.6 VIP: Lisans sunucusu (bycomsolutions.in) kapalı olduğu için kontrol devre dışı.
-        // Uygulama artık her zaman "lisanslı" ve "yetkili" modda devam edecek.
+        // GoatBorg v2.6 VIP: Lisans sunucusu patladığı için kontrol devre dışı bırakıldı.
+        // Uygulama artık her zaman "lisanslı" modda devam eder.
     }
 }
